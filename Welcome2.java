bbd8fd1bb2557c4d68f2faffe98484216b23f0d1
Elizeu Figueiredo <168204973+figueiredoelizeu@users.noreply.github.com>
2024-11-26 06:24:53 -0300

    Add files via upload

---
+++ Welcome2.java
@@ -0,0 +1,10 @@
+//Imprimir linha de texto com multiplas funções
+
+public class Welcome2
+{
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("Programação em Java");
+    }
+}
+
